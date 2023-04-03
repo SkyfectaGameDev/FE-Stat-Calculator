a48e3cb622bdf8e537cd74d4e6ce7a57f035b7db
SkyfectaGameDev <skyfecta@gmail.com>
2023-04-03 14:37:55 +0100

    0.23.0403c  - Character Profile Basics
    - Added dropdown select lists
    - Added dropdown list contents
    - Added character name

--- Project/App/FEStats/App.js
+++ Project/App/FEStats/App.js
@@ -4,20 +4,87 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 import portrait from './assets/images/portraits/shadowDragon/Marth_Portrait_FE11.webp'
 import { SelectList } from 'react-native-dropdown-select-list';
 
-// ---------- App Structure (HTML) ----------
+
 export default function App() {
+  
+  //#region ---------- Dropdown Menus ----------
+  
+  // - Selected State -
+  const [selected, setSelected] = React.useState("");
+
+  // - Menus & Data -
+  const data1 = [
+    {key:'1', value:'Box 1, Option 1'},
+    {key:'1', value:'Box 1, Option 2'},
+    {key:'1', value:'Box 1, Option 3'},
+    {key:'1', value:'Box 1, Option 4'},
+    {key:'1', value:'Box 1, Option 5'},
+  ]
+  const data2 = [
+    {key:'1', value:'Box 2, Option 1'},
+    {key:'1', value:'Box 2, Option 2'},
+    {key:'1', value:'Box 2, Option 3'},
+    {key:'1', value:'Box 2, Option 4'},
+    {key:'1', value:'Box 2, Option 5'},
+  ]
+  const data3 = [
+    {key:'1', value:'Box 3, Option 1'},
+    {key:'1', value:'Box 3, Option 2'},
+    {key:'1', value:'Box 3, Option 3'},
+    {key:'1', value:'Box 3, Option 4'},
+    {key:'1', value:'Box 3, Option 5'},
+  ]
+  //#endregion
+
+  //#region ---------- App Structure (HTML) ----------
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>FEStats</Text>
+      <Text style={styles.title}>FEStats Alpha 0.23.0403c</Text>
       <View style={styles.characterProfile}>
-        <Image style={styles.characterPortrait} source={portrait} alt="Placeholder Portrait"></Image>
+        <View>
+          <Image style={styles.characterPortrait} source={portrait} alt="Placeholder Portrait"></Image>
+          <Text style={styles.characterName}>Marth</Text>
+        </View>
+        <View style={styles.characterDropdown}>
+          {/* ----- Dropdown Menu 1 ----- */}
+          <SelectList
+            data={data1}
+            setSelected={setSelected}
+            boxStyles={{backgroundColor: '#fff', width: 210, height: 40}}
+            inputStyles={{fontSize: 10}}
+            dropdownStyles={{backgroundColor: '#fff'}}
+            dropdownTextStyles={{fontSize: 10}}
+            >
+          </SelectList>
+          {/* ----- Dropdown Menu 2 ----- */}
+          <SelectList
+            data={data2}
+            setSelected={setSelected}
+            boxStyles={{backgroundColor: '#fff', width: 210, height: 40}}
+            inputStyles={{fontSize: 10}}
+            dropdownStyles={{backgroundColor: '#fff'}}
+            dropdownTextStyles={{fontSize: 10}}
+            >
+          </SelectList>
+          {/* ----- Dropdown Menu 3 ----- */}
+          <SelectList
+            data={data3}
+            setSelected={setSelected}
+            boxStyles={{backgroundColor: '#fff', width: 210, height: 40}}
+            inputStyles={{fontSize: 10}}
+            dropdownStyles={{backgroundColor: '#fff'}}
+            dropdownTextStyles={{fontSize: 10}}
+            >
+          </SelectList>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
   );
+  //#endregion
 }
 
-// ---------- App Styling (CSS) ----------
+//#region ---------- App Styling (CSS) ----------
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,7 +100,14 @@ const styles = StyleSheet.create({
 
   characterProfile: {
     display: 'flex',
+    flexDirection: 'row',
     padding: 20,
+    gap: 10
+  },
+
+  characterPortraitContainer: {
+    display: 'flex',
+    flexDirection: 'column',
   },
 
   characterPortrait: {
@@ -45,7 +119,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 
-  characterDropdown: {
+  characterName: {
+    color: '#fff',
+    textAlign: 'center'
+  },
 
+  characterDropdown: {
+    display: 'flex',
+    flexDirection: 'column',
   }
 });
+//#endregion
+
+
+/*#region ---------- Credits ----------
+
+Programming:            Skyfecta
+Logic & Math:           AuroraHertz
+Dropdown Select List:   https://github.com/danish1658/react-native-dropdown-select-list
+
+#endregion */
